85721a26c30cd1464fdcf0040e0d7216f5d0cc61
ozlemsalehi <44723115+ozlemsalehi@users.noreply.github.com>
2018-11-03 16:08:15 +0300

    Create q2.c

---
+++ Lab/Lab6/q2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+  int i,j,n;
+  scanf("%d",&n);
+
+  for(i=1;i<=n;i++){
+    for(j=1;j<=i-1;j++){
+      printf(" ");
+    }
+    for(j=n;j>=i;j--){
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+  //alternative solution
+  for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+      if(j<i){
+        printf(" ");
+       }
+       else{
+        printf("*");
+       }
+      }
+     printf("\n");
+    }
+  
+  return 0;
+}
