89eaa00e98f7a0e2dde74c0706857cff73c7688a
ozlemsalehi <44723115+ozlemsalehi@users.noreply.github.com>
2018-11-03 13:09:52 +0300

    Create quiz1q2.c

---
+++ Quiz/quiz1q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <teachingcodes.h>
+
+
+int main(void) {
+	TC_BEGIN;
+
+
+	// DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+	int a,b,c;
+	scanf("%d%d%d", &a, &b, &c);
+	printf("%.1f", (a+b+c)/(3.0));
+
+	// DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+	TC_END;
+	return 0;
+}
