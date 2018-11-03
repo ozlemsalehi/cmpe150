3e52eb679171486bf6b36a09eb6d7851722b3449
ozlemsalehi <44723115+ozlemsalehi@users.noreply.github.com>
2018-11-03 13:09:05 +0300

    Create quiz1q1.c

---
+++ Quiz/quiz1q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <teachingcodes.h>
+
+
+int main(void) {
+	TC_BEGIN;
+
+	// DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+	char c;
+	scanf("%c", &c);
+	printf("%c", c + 'A'-'a');
+	// DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+	TC_END;
+	return 0;
+}
