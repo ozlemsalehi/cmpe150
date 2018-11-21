29d9592150d1807bcda605e671bdb7b8ccb6200f
ozlemsalehi <44723115+ozlemsalehi@users.noreply.github.com>
2018-11-22 02:49:57 +0300

    Create ex1.c

---
+++ Lab/Lab9/ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define square(x) (x)*(x)
+
+
+// DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+int main(void) {
+
+
+	int n;
+	scanf("%d", &n);
+
+	printf("%d", square(n));
+	
+	return 0;
+
+}
+
