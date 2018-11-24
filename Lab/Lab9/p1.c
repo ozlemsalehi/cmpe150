2ab176f14d0084001ef6dc8f9440434e34839b38
ozlemsalehi <44723115+ozlemsalehi@users.noreply.github.com>
2018-11-25 01:02:53 +0300

    Update p1.c

--- Lab/Lab9/p1.c
+++ Lab/Lab9/p1.c
@@ -1,54 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int reverse(int n){
-
-    int number=0;
-    while(n>0){
-        number=number*10+n%10;
-        n=n/10;
-    }
-    return number;
-}
-
 
 int binary(int n){
 
+    int digit=1;
     int number=0;
-
     while(n>0){
-        number=number*10+n%2;
-        n=n/2;
+	number+=n%2*digit;
+	n=n/2;
+	digit*=10;  
     }
 
-    return reverse(number);
-}
-
-int binary2(int n){
-
-	int digit=1;
-	int number=0;
-	while(n>0){
-		number+=n%2*digit;
-	    n=n/2;
-	    digit*=10;
-	}
-
-	return number;
+    return number;
 
 }
 
 
-int main(void) {
+int main(void) { //Convert number to binary
 
 
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-    printf("%d\n",binary(n));
-    printf("%d",binary2(n));
+    printf("%d\n",binary(n)); 
 
-	return 0;
+    return 0;
 
 }
 
